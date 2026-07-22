719acc2fd2d1089c7b7ba5fa55ee214684c41438
BROGOTSOME <ishaanjpw@gmail.com>
2026-07-22 20:58:42 +0530

    add:Container:Pull image

---
+++ evalution-service/src/containers/pullimage.container.ts
@@ -0,0 +1,36 @@
+import Docker from "dockerode";
+import logger from "../config/logger.config";
+import {PYTHON_IMAGE,CPP_IMAGE} from "../utils/constants";
+
+async function pullimg(img:string){
+    const docker = new Docker();
+    return new Promise((res,rej)=>{
+       docker.pull(img,(err:Error,stream:NodeJS.ReadableStream)=>{
+            if(err){
+                rej(err);
+                return;
+            }
+            docker.modem.followProgress(
+                stream,
+                function onFinished(finalErr,output){
+                    if(finalErr) return rej(finalErr);
+                    res(output)
+                },
+                function onProgress(event) {
+                    logger.info(event.status);
+                }
+            );
+       });
+    });
+}
+export async function pullAllImages(){
+    const images=[PYTHON_IMAGE,CPP_IMAGE];
+    const promises=images.map(image=>pullimg(image));
+    try{
+        await Promise.all(promises);
+        logger.info("All of The neccessary Image Pulled succesfully");
+    }
+    catch(err){
+        logger.error("Error Pulling Images",err);
+    }
+}
--- evalution-service/src/utils/constants.ts
+++ evalution-service/src/utils/constants.ts
@@ -1,1 +1,4 @@
 export const SUBMISSION_QUEUE: string="submission";
+//todo GCC image is heavy 1 gb image , update it with efficient->debian:bookworm-slim + then gcc complier installation -> creating custom cpp intallation support pipline  
+export const CPP_IMAGE: string="gcc:14";
+export const PYTHON_IMAGE:string="python:3.12-slim";
